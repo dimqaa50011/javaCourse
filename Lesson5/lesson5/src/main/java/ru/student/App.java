ff8c0d27a234c26cf03860090b1b4ed313e176da
Dmitry <dz5011@yandex.ru>
2022-12-10 10:04:46 +0300

    basic lesson5 edit phone book. add removing phone and edit removing user

--- Lesson5/lesson5/src/main/java/ru/student/App.java
+++ Lesson5/lesson5/src/main/java/ru/student/App.java
@@ -1,24 +1,24 @@
 package ru.student;
 
-/**
- * Hello world!
- *
- */
+public class App {
+    public static void main(String[] args) {
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
         phomeBookStarter();
     }
+
     private static void phomeBookStarter() {
         PhoneBook pb = new PhoneBook();
         pb.getAll();
         pb.addNewContact("Dimqa", 123, 456, 789);
-        pb.getAll();
+
         pb.addNewPhone("Diqa", 12456);
         pb.getAll();
-        pb.getOneUser("Dimqa");
+        System.out.println(pb.getOneUser("Dimqa"));
+
+        pb.removePhone("Dimqa", 0);
+        pb.getAll();
+        pb.removeUser("Diqa");
+
+        pb.getAll();
     }
 }
--- Lesson5/lesson5/src/main/java/ru/student/PhoneBook.java
+++ Lesson5/lesson5/src/main/java/ru/student/PhoneBook.java
@@ -10,8 +10,13 @@ import java.util.Map;
 public class PhoneBook {
     private Map<String, List<Integer>> phoneBook = new HashMap<>();
 
-    public Map<String, List<Integer>> getAll() {
-        return phoneBook;
+    public void getAll() {
+        String msg;
+        if (phoneBook.isEmpty())
+            msg = "Телефонная книга пуста. Сначала ее нужно наполнить";
+        else
+            msg = String.format("Пользователи в вашей телефонной книге:\n%s", phoneBook);
+        System.out.println(msg);
     }
 
     public List<Integer> getOneUser(String name) {
@@ -41,6 +46,44 @@ public class PhoneBook {
             phonesList.add(phone);
         }
         return phonesList;
+    }
+
+    public void removeUser(String name) {
+        String msg;
+        if (deleteUser(name))
+            msg = String.format("Пользователь %s удален", name);
+        else
+            msg = String.format("Пользователя %s несуществует", name);
+        System.out.println(msg);
+    }
+
+    private boolean deleteUser(String name) {
+        if (phoneBook.containsKey(name)) {
+            phoneBook.remove(name);
+            return true;
+        }
+        return false;
+    }
+
+    public void removePhone(String name, int index) {
+        String msg;
+        int result = deletePhone(name, index);
+        if (result != -1)
+            msg = String.format("Телефон %d пользователя %s удален", result, name);
+        else
+            msg = String.format("Пользователя %s несуществует", name);
+        System.out.println(msg);
+    }
+
+    private int deletePhone(String name, int index) {
+        if (phoneBook.containsKey(name)) {
+            List<Integer> userPhones = phoneBook.get(name);
+            int phone = userPhones.get(index);
+            userPhones.remove(index);
+            phoneBook.put(name, userPhones);
+            return phone;
+        }
+        return -1;
     }
 
 }
